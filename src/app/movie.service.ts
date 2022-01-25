cecc694f24989e54476914968074b1d878bacb3f
Isabella <dorighetta007@gmail.com>
2022-01-24 21:22:29 -0300

    url change

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MovieService {
 
-  movieUrl = "http://127.0.0.1:8000/admin/movies";
+  movieUrl = "http://127.0.0.1:8000/api/movies";
 
   constructor(private http : HttpClient) { }
 
